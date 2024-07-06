3c5a8cc787bb7b4196509a7e25227974bf8d94eb
Shay Levi <shay2301@gmail.com>
2024-07-06 12:45:09 +0300

    Data and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fairness_pipeline_bank_marketing",
+    version="0.1.0",
+    author="Shay",
+    packages=find_packages(),
+    install_requires=[
+        # Add your project dependencies here
+    ],
+)
